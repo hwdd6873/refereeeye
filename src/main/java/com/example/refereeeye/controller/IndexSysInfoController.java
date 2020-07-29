be242c79705ece486b240c0b518a2166dee05e03
hwdd6873 <hwdd6873@gmail.com>
2020-07-29 17:42:17 +0800

    首页系统资源状态接口

--- src/main/java/com/example/refereeeye/controller/IndexSysInfoController.java
+++ src/main/java/com/example/refereeeye/controller/IndexSysInfoController.java
@@ -1,14 +1,18 @@
 package com.example.refereeeye.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexSysInfoController {
 
+    private static Logger logger  = LoggerFactory.getLogger(IndexSysInfoController.class);
+
     @PostMapping("/index/sysInfo")
     public void systemInfoIndex(){
-
+        logger.info("访问系统状态接口");
     }
 
 }
