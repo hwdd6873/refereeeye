91dea2ee795d240cd986cd68203050100cf8b988
hwdd6873 <hwdd6873@gmail.com>
2020-07-29 17:31:51 +0800

    首页系统状态值统计

---
+++ src/main/java/com/example/refereeeye/controller/IndexSysInfoController.java
@@ -0,0 +1,14 @@
+package com.example.refereeeye.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexSysInfoController {
+
+    @PostMapping("/index/sysInfo")
+    public void systemInfoIndex(){
+
+    }
+
+}
