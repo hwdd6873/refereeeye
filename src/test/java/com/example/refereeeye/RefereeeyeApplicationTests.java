3f2199ce0ce091d18fa3fa70e89b8888024689e8
hwdd6873 <hwdd6873@gmail.com>
2020-07-29 17:08:56 +0800

    初始化提交

---
+++ src/test/java/com/example/refereeeye/RefereeeyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.refereeeye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefereeeyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
